4eb9dcc288db88dffa41502b42f6be7bbd9b1616
Bram Lo <bram_lo@outlook.com>
2022-04-02 15:54:06 +0200

    Remove autocmd for tectonic
    Vimtex with latexmk works by running :VimtexCompile. Saving
    the buffer will update the pdf.

--- nvim/.config/nvim/after/plugin/vimtex.lua
+++ nvim/.config/nvim/after/plugin/vimtex.lua
@@ -4,11 +4,3 @@ g.vimtex_format_enabled = 1
 -- The quickfix window is never opened/closed automatically.
 g.vimtex_quickfix_mode = 0
 g.vimtex_view_method = "zathura"
-
--- Build tex files on file save.
-_ = vim.cmd [[
-  augroup CompileAfterSave
-    au!
-    au BufWritePost *.tex :VimtexCompile
-  augroup end
-]]
