11dcd0fabc41c6002dfb9160dc0db784191d6e04
Bram Lo <bram_lo@outlook.com>
2022-06-16 01:45:35 +0200

    Update nvim configs
      - Set borders to separate float windows from open buffer
      - Disable copilot for .tex files
      - Remove line in picom configuration causing error

--- nvim/.config/nvim/after/plugin/vimtex.lua
+++ nvim/.config/nvim/after/plugin/vimtex.lua
@@ -4,13 +4,17 @@ g.vimtex_format_enabled = 1
 -- The quickfix window is never opened/closed automatically.
 g.vimtex_quickfix_mode = 0
 g.vimtex_view_method = "zathura"
+local disable_copilot = "Copilot disable"
 
--- Disable diagnostics when editing .tex files. Diagnostics aren't that useful
--- when writing latex and it causes a lot of distracting warnings -- even when
--- tex compiles perfectly.
+-- Disable diagnostics and github copilot when editing .tex files.
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.tex" },
   callback = function()
     vim.diagnostic.disable()
   end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.tex" },
+  command = disable_copilot, -- Or myvimfun
 })
