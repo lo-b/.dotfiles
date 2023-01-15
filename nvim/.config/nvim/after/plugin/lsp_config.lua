0c805bfebc12e375c763bce9e3d8d071d301c3ad
Bram Lo <bram_lo@outlook.com>
2023-01-15 22:07:04 +0100

    Replace deprecated nvim plugin APIs
    Change deprecated plugin APIs with tips from shown in std :messages
    output.

--- nvim/.config/nvim/after/plugin/lsp_config.lua
+++ nvim/.config/nvim/after/plugin/lsp_config.lua
@@ -17,7 +17,7 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 end
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
+local capabilities = require("cmp_nvim_lsp").default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 
--- nvim/.config/nvim/after/plugin/null_ls.lua
+++ nvim/.config/nvim/after/plugin/null_ls.lua
@@ -55,7 +55,7 @@ require("null-ls").setup {
         buffer = bufnr,
         callback = function()
           -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.formatting_seq_sync()
+          vim.lsp.buf.format()
         end,
       })
     end
