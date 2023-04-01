78a8b15bbd9b6b329209bec766c04f6be40c68ed
Bram Lo <bram_lo@outlook.com>
2023-04-01 18:24:04 +0200

    Update config files and install programs
      - Update old vimtex config, disabling copilot.
      - Install steam to try out gaming on my old latop.
      - Install tlp for powersaving and powertop to see what's sucking
        juice.
      - Setup bluetooth to connect Galaxy Buds 2 Pro. Replacing pulseaudio with
        pipewire was recommended. Also added 'blueman', a bluetooth applet.

--- nvim/.config/nvim/after/plugin/vimtex.lua
+++ nvim/.config/nvim/after/plugin/vimtex.lua
@@ -4,7 +4,6 @@ g.vimtex_format_enabled = 1
 -- The quickfix window is never opened/closed automatically.
 g.vimtex_quickfix_mode = 0
 g.vimtex_view_method = "zathura"
-local disable_copilot = "Copilot disable"
 
 -- Disable diagnostics and github copilot when editing .tex files.
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
@@ -12,9 +11,4 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   callback = function()
     vim.diagnostic.disable()
   end,
-})
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  pattern = { "*.tex" },
-  command = disable_copilot, -- Or myvimfun
 })
