6c859107b697dc22f309a23e8127b47419d1b6c0
lo-b <bram_lo@outlook.com>
2024-08-18 19:13:45 +0200

    update nvim-tree & octo configs
    Update octo (github plugin) & nvim-tree configs, removing warnings.

--- nvim/.config/nvim/lua/bl/plugins.lua
+++ nvim/.config/nvim/lua/bl/plugins.lua
@@ -31,7 +31,9 @@ local plugins = {
       "kyazdani42/nvim-web-devicons",
     },
     config = function()
-      require("octo").setup()
+      require("octo").setup {
+        default_to_projects_v2 = true,
+      }
     end,
   },
   "pearofducks/ansible-vim",
